101f4b662bcadc6531c1f684af46148c9078a7bb
kingnnt <dev.kingnnt@gmail.com>
2024-11-19 14:04:38 +0700

    feat: flatten json message

--- src/libraries/intl/intl.ts
+++ src/libraries/intl/intl.ts
@@ -1,12 +1,13 @@
 import { createIntl, createIntlCache } from "react-intl";
 import EnUS from "@lang/en.json";
 import { LocaleSupport } from "@/enums";
+import { flatten } from "flat";
 
 const intlCache = createIntlCache();
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export const messages: Record<LocaleSupport, any> = {
-  en: EnUS,
+  en: flatten(EnUS),
 };
 
 export const createIntlInstance = (locale: LocaleSupport) =>
